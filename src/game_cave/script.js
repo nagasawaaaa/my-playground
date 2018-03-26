b4442ba103603bd67a8eb85e800a2d4f4106e36d
naga <snagasaw.sp.create@gmail.com>
2018-03-26 11:58:37 +0900

    addEventListner load -> DOMContentLoaded に変更
    - 各行にコメントを追加

--- src/game_cave/script.js
+++ src/game_cave/script.js
@@ -5,9 +5,10 @@
   var walls = [], slope = 0, timer, ship, main;
 
   function init() {
-    main = document.getElementById('main');
-    ship = document.getElementById('ship');
+    main = document.getElementById('main'); // ゲーム領域
+    ship = document.getElementById('ship'); // player
 
+    // 洞窟の通り道を作成
     for (var i = 0 ; i < WALLS ; i++) {
       walls[i] = document.createElement("div");
       walls[i].style.position = "absolute";
@@ -19,45 +20,56 @@
       main.appendChild(walls[i]);
     }
 
-    slope = Math.floor(Math.random() * 5) + 1;
-    timer = setInterval(mainloop, 50);
+    slope = Math.floor(Math.random() * 5) + 1;  // 勾配の度合いを計算
+    timer = setInterval(mainloop, 50); // 50ミリ秒毎に mainloop() を発火
+
+    // 何かしらキーが押されているかどうかのイベントを取得
     window.addEventListener('keydown', function () { keyDown = true; });
     window.addEventListener('keyup', function () { keyDown = false; });
   }
 
+  // 衝突判定
   function hitTest(){
-    var st = parseInt(ship.style.top) + 10;
-    var sh = parseInt(ship.style.height);
-    var sb = st + sh - 20;
+    var st = parseInt(ship.style.top) + 10; // ship top
+    var sh = parseInt(ship.style.height); // ship height
+    var sb = st + sh - 20; // ship bottom
 
-    var wt = parseInt(walls[14].style.top);
-    var wh = parseInt(walls[14].style.height);
-    var wb = wh + wt;
+    var wt = parseInt(walls[14].style.top);  // wall top
+    var wh = parseInt(walls[14].style.height);  // wall height
+    var wb = wh + wt;  // wall bottom
 
+    // shipの上端が洞窟の上端より小さい or shipの下端が洞窟の下端より大きい場合に true を返す
     return (st < wt) || (sb > wb );
   }
 
   function mainloop() {
+    // 自機が壁にぶつかったら
     if (hitTest()) {
+      // timerを止めて
       clearInterval(timer);
+      // 爆発画像を表示して
       document.getElementById('bang').style.top = (y - 40) + "px";
       document.getElementById('bang').style.visibility = "visible";
+      // 終了
       return;
     }
 
-    score += 10;
-    document.getElementById('score').innerHTML = score.toString();
+    score += 10;  // 50ms毎に 10pづつ加算
+    document.getElementById('score').innerHTML = score.toString();  // Score欄に記入
 
-    v += keyDown ? -3 : 3;
-    y += v;
-    ship.style.top = y + 'px';
+    // velocity(速度) yard(距離？)
+    v += keyDown ? -3 : 3;  // 速度を更新 キーが押されると自機が上昇するので 負の値を加算する
+    y += v;  // 速度を距離に反映
+    ship.style.top = y + 'px';  // 自機の位置
 
-    var edge = walls[WALLS - 1].style;
-    var t = parseInt(edge.top);
-    var h = parseInt(edge.height);
-    var b = h + t;
-    t += slope;
-    b += slope;
+    var edge = walls[WALLS - 1].style;  // 右端の洞窟のスタイル
+    var t = parseInt(edge.top);;  // 右端の洞窟の上端
+    var h = parseInt(edge.height);  // 右端の洞窟の高さ
+    var b = h + t;  // 右端の洞窟の下端
+    t += slope; // 勾配を洞窟の上端に追加
+    b += slope; // 勾配を洞窟の下端に追加
+
+    // 勾配が上端か下端に到達したかを判定
     if ((t < 0) && (slope < 0) || (b > 600) && (slope > 0)) {
       slope = (Math.floor(Math.random() * 5) + 1) * (slope < 0 ? 1 : -1);
       edge.top = (t + 10) + "px";
@@ -66,11 +78,12 @@
       edge.top = t + "px";
     }
 
+    // 横スクロールを実装
     for (var i = 0 ; i < WALLS - 1 ; i++) {
       walls[i].style.top = walls[i + 1].style.top;
       walls[i].style.height = walls[i + 1].style.height;
     }
   }
 
-  window.addEventListener('load', init, true);
+  window.addEventListener('DOMContentLoaded', init, true);
 }());
